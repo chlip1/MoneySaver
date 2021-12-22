e68c405ffcc0b6a8aabd6b81f6c88a2bc5f43761
Łukasz Chlipała <lukasz.chlipala3@gmail.com>
2021-12-22 12:01:20 +0100

    new file with menu gui
    Signed-off-by: Łukasz Chlipała <lukasz.chlipala3@gmail.com>

--- main.py
+++ main.py
@@ -1,25 +1,8 @@
 from tkinter import *
+from menu import *
 from database import *
 
 if __name__ == "__main__":
 
-    #SETTINGS
-    menu = Tk()
-    menu.configure(bg="deep sky blue")
-    menu.title("MENU")
-    menu.geometry("230x240")
-
-    Label(menu, text="Hello!", font=('Helvetica 20 bold'), bg="deep sky blue").grid(row=0, column=0, padx=0, pady=0)
-
-    #INIT BUTTONS
-    button_database = Button(menu, text="Add a expenditure", bg="gray", height=3, width=30)
-    button_visualisation = Button(menu, text="Show past expenditures", bg="gray", height=3, width=30)
-    button_exit = Button(menu, text="EXIT", bg="gray", height=3, width=30)
-
-    button_database.grid(row = 1, column = 0, padx = 5, pady = 2)
-    button_visualisation.grid(row = 2, column = 0, padx = 5, pady = 2)
-    button_exit.grid(row = 3, column = 0, padx =5, pady = 2)
-
-    menu.mainloop()
-
-    ##gui_database_creator()
+    menu_init()
+    gui_database_creator()
---
+++ menu.py
@@ -0,0 +1,31 @@
+from tkinter import *
+
+def menu_settings(menu):
+    # SETTINGS
+    menu.configure(bg="deep sky blue")
+    menu.title("MENU")
+    menu.geometry("230x240")
+
+def menu_buttons(menu):
+    # INIT BUTTONS
+    button_database = Button(menu, text="Add a expenditure", bg="gray", height=3, width=30)
+    button_visualisation = Button(menu, text="Show past expenditures", bg="gray", height=3, width=30)
+    button_exit = Button(menu, text="EXIT", bg="gray", height=3, width=30)
+
+    button_database.grid(row=1, column=0, padx=5, pady=2)
+    button_visualisation.grid(row=2, column=0, padx=5, pady=2)
+    button_exit.grid(row=3, column=0, padx=5, pady=2)
+
+def menu_labels(menu):
+    # INIT LABEL
+    Label(menu, text="Hello!", font=('Helvetica 20 bold'), bg="deep sky blue").grid(row=0, column=0, padx=0, pady=0)
+
+def menu_init():
+    #INIT tk
+    menu = Tk()
+    #USE SELFMADE FUNCTION
+    menu_settings(menu)
+    menu_buttons(menu)
+    menu_labels(menu)
+    #START
+    menu.mainloop()
