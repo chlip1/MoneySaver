2859ea3ade92ff9754a92245ea5a3d4c4b92585d
Łukasz Chlipała <lukasz.chlipala3@gmail.com>
2021-12-22 10:54:03 +0100

    Ustawienia początkowe menu
    Signed-off-by: Łukasz Chlipała <lukasz.chlipala3@gmail.com>

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-git
+from tkinter import *
+
+if __name__ == "__main__":
+
+    menu = Tk()
+    menu.configure(bg="deep sky blue")
+    menu.title("MENU")
+    menu.mainloop()
+    ###njkjkjkjjjjhhjjkhjjkjkjk
