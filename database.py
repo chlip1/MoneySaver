8d673b0d69f92798323598c66cbc16888c54a32d
Łukasz Chlipała <lukasz.chlipala3@gmail.com>
2021-12-23 13:14:17 +0100

    Zamykanie się okna z MENU oraz poprawki bazy danych, dodanie paru funkcjonalnosci
    Signed-off-by: Łukasz Chlipała <lukasz.chlipala3@gmail.com>

--- database.py
+++ database.py
@@ -6,13 +6,15 @@ def init_database():
 
     try:
         #CREATE A DATABASE OR CONNECT TO ONE
-        conn = sqlite3.connect('name1.db')
+        conn = sqlite3.connect('name.db')
         c = conn.cursor()
 
         #CREATE TABLE
         c.execute("""CREATE TABLE expenditures(
                 amount integer,
-                month text
+                month text,
+                category text,
+                descriptions text
                 )""")
 
         conn.commit()
@@ -23,7 +25,7 @@ def init_database():
 
 def show_records():
 
-    conn = sqlite3.connect('name1.db')
+    conn = sqlite3.connect('name.db')
     c = conn.cursor()
 
     c.execute("SELECT *, oid FROM expenditures")
@@ -33,74 +35,74 @@ def show_records():
     conn.commit()
     conn.close()
 
-def submit(amount, month_clicked):
+def submit(amount, month_clicked, category_clicked, descriptions):
 
-    conn = sqlite3.connect('name1.db')
+    conn = sqlite3.connect('name.db')
     c = conn.cursor()
 
-    c.execute("INSERT INTO expenditures VALUES(:amount, :month)",{'amount': amount.get(),'month': month_clicked.get()})
+    c.execute("INSERT INTO expenditures VALUES(:amount, :month, :category, :descriptions)",{'amount': amount.get(),
+                                                                  'month': month_clicked.get(),
+                                                                  'category':category_clicked.get(),
+                                                                  'descriptions': descriptions.get()})
 
     conn.commit()
     conn.close()
 
     amount.delete(0, END)
+    descriptions.delete(0, END)
 
-def gui_db_button(gui_db, amount, month_clicked):
-
-    button_submit = Button(gui_db, text="Submit", bg="gray", height=1, width=15, command=lambda:submit(amount, month_clicked))
-    button_submit.grid(row=6, column=0, padx=5, pady=2)
-
-def gui_db_dropDownMenu(gui_db):
-    '''
-    month_clicked = StringVar(gui_db)
-    month_clicked.set("January")
-
-    month = OptionMenu(gui_db, month_clicked, "January", "February", "March", "April", "May")
-    month.grid(row=1, column=0)
-    '''
-    category_clicked = StringVar(gui_db)
-    category_clicked.set("Food")
-
-    category = OptionMenu(gui_db, category_clicked, "January", "February", "March", "April", "May")
-    category.grid(row=3, column=0)
-
-
-def gui_db_Label(gui_db):
-
-    Label(gui_db, text="Month", font=('Helvetica 11 bold'), bg="lightgray").grid(row=0, column=0, padx=0, pady=0)
-    Label(gui_db, text="Category", font=('Helvetica 11 bold'), bg="lightgray").grid(row=2, column=0, padx=0, pady=0)
-    Label(gui_db, text="Amount", font=('Helvetica 11 bold'), bg="lightgray").grid(row=4, column=0, padx=0, pady=0)
-
-'''
-def gui_db_Entry(gui_db):
-
-    amount = Entry(gui_db, width=30)
-    amount.grid(row=5, column=0, padx=20, pady=5)
-'''
 def gui_database_creator(gui_db):
 
     gui_db.configure(bg="lightgray")
     gui_db.title("Add a expenditure")
-    gui_db.geometry("230x200")
+    gui_db.geometry("230x280")
 
-def database_main():
+def gui_Functionalities(gui_db):
 
-    init_database()
-    gui_db = Tk()
-    gui_database_creator(gui_db)
-    gui_db_dropDownMenu(gui_db)
+    Label(gui_db, text="Month", font=('Helvetica 11 bold'), bg="lightgray").grid(row=0, column=0, padx=0, pady=0)
+    Label(gui_db, text="Category", font=('Helvetica 11 bold'), bg="lightgray").grid(row=2, column=0, padx=0, pady=0)
+    Label(gui_db, text="Amount", font=('Helvetica 11 bold'), bg="lightgray").grid(row=4, column=0, padx=0, pady=0)
+    Label(gui_db, text="Descriptions", font=('Helvetica 11 bold'), bg="lightgray").grid(row=6, column=0, padx=0, pady=0)
 
     month_clicked = StringVar(gui_db)
     month_clicked.set("January")
 
-    month = OptionMenu(gui_db, month_clicked, "January", "February", "March", "April", "May")
+    month = OptionMenu(gui_db, month_clicked, "January", "February", "March", "April", "May",
+                       "June", "July", "August", "September", "October", "November", "December")
     month.grid(row=1, column=0)
+
+    category_clicked = StringVar(gui_db)
+    category_clicked.set("Food from restaurant")
+
+    category = OptionMenu(gui_db, category_clicked, "Grocery shopping", "Food from restaurant", "Car fuel",
+                          "Car extra expenses", "Investments", "Gifts and loan", "Clothes", "House", "Subscription")
+    category.grid(row=3, column=0)
 
     amount = Entry(gui_db, width=30)
     amount.grid(row=5, column=0, padx=20, pady=5)
-    #gui_db_Entry(gui_db)
-    gui_db_button(gui_db, amount ,month_clicked)
-    gui_db_Label(gui_db)
+
+    descriptions = Entry(gui_db, width=30)
+    descriptions.grid(row=7, column=0, padx=20, pady=5)
+
+    button_submit = Button(gui_db, text="Submit", bg="gray", height=1, width=15,
+                           command=lambda: submit(amount, month_clicked, category_clicked, descriptions))
+    button_submit.grid(row=8, column=0, padx=5, pady=2)
+
+    button_exit = Button(gui_db, text="Exit", bg="gray", height=1, width=15,
+                           command=gui_db.quit)
+    button_exit.grid(row=9, column=0, padx=5, pady=2)
+
+def database_main(menu):
+
+    init_database()
+    gui_db = Tk()
+
+    menu.destroy()
+
+    gui_database_creator(gui_db)
+
+    gui_Functionalities(gui_db)
+
     show_records()
     gui_db.mainloop()
 
--- main.py
+++ main.py
@@ -6,13 +6,13 @@ def menu_settings(menu):
     # SETTINGS
     menu.configure(bg="lightgray")
     menu.title("MENU")
-    menu.geometry("230x240")
+    menu.geometry("230x150")
 
 def menu_buttons(menu):
     # INIT BUTTONS
-    button_database = Button(menu, text="Add a expenditure", bg="gray", height=3, width=30, command=database_main)
-    button_visualisation = Button(menu, text="Show past expenditures", bg="gray", height=3, width=30, command=gui_visualisation_creator)
-    button_exit = Button(menu, text="EXIT", bg="gray", height=3, width=30, command=menu.quit)
+    button_database = Button(menu, text="Add a expenditure", bg="gray", height=1, width=30, command=lambda: database_main(menu))
+    button_visualisation = Button(menu, text="Show past expenditures", bg="gray", height=1, width=30, command=gui_visualisation_creator)
+    button_exit = Button(menu, text="EXIT", bg="gray", height=1, width=30, command=menu.quit)
 
     button_database.grid(row=1, column=0, padx=5, pady=2)
     button_visualisation.grid(row=2, column=0, padx=5, pady=2)
